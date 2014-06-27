01bf22f167a92637d3efb594ee72ad24b50dbf1a
Alexander Nilsson <alexander.nilsson83@gmail.com>
2014-06-27 16:23:12 +0200

    bla

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,9 +1,10 @@
 app.controller('HomeController',['$scope','mongoService', '$location', '$routeParams', function($scope, mongoService, $routeParams, $location) {
   
   $scope.showPosts = mongoService.index();
-  //$scope.showPosts = [mongoService.show()];
+  
   $scope.deletePost = function(postID){
     console.log(postID);
+
     mongoService.destroy(postID);
   }
 
